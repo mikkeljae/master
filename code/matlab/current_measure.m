a43eb29019c49ee608cf631e8747f4918a7cf18d
Mikkel <mikkel.jae@gmail.com>
2017-11-13 10:15:47 +0100

    all data from controllerboard tests

--- code/matlab/current_measure.m
+++ code/matlab/current_measure.m
@@ -8,12 +8,7 @@ G = 100;
 Vmax = G * I_stall * R
 
 
-R2 = 500e-6;
-G2 = 50;
-Vmax2 = G2 * I_stall * R2
-
-
-
 I_max_meas = V_max_meas/(R*G)
-
-I_max_meas2 = V_max_meas/(R2*G2)
+ 
+Ii = 15;
+Vi = G *Ii * R
