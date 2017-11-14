e647893c30bb1c290124a4eb18b01be70d6dad59
Mikkel <mikkel.jae@gmail.com>
2017-11-14 13:53:42 +0100

    lots of plotting

---
+++ code/matlab/plot_current_motor_supply/plot_current_motor_suppy.m
@@ -0,0 +1,56 @@
+clear all; close all; clc;
+
+to_tikz = 1;
+
+C1_s = csvread('C1controller-board2-supplycurrent-gate-22k-50d0d00000.txt',5,0);    %TEMP
+C1_m = csvread('C1controller-board2-motorcurrent-gate-22k-50d0d00000.txt',5,0);    %TEMP
+
+C2_s = csvread('C2controller-board2-supplycurrent-gate-22k-50d0d00000.txt',5,0);    %TEMP
+C2_m = csvread('C2controller-board2-motorcurrent-gate-22k-50d0d00000.txt',5,0);    %TEMP
+
+C4_supplycurrent = csvread('C4controller-board2-supplycurrent-gate-22k-50d0d00000.txt',5,0);
+
+C4_motorcurrent = csvread('C4controller-board2-motorcurrent-gate-22k-50d0d00000.txt',5,0);
+
+timeshift = 0.7e-4;
+phase = -2.27e-5;
+time_supplycurrent_C4 = C4_supplycurrent(:,1) + timeshift;
+supplycurrent_C4 = C4_supplycurrent(:,2)*-1;
+
+time_motorcurrent_C4 = C4_motorcurrent(:,1) + timeshift + phase;
+motorcurrent_C4 = C4_motorcurrent(:,2)*-1;
+
+%TEMP
+time_C1_s = C1_s(:,1) + timeshift;
+data_C1_s = C1_s(:,2);
+%TEMP
+time_C1_m = C1_m(:,1) + timeshift + phase;
+data_C1_m = C1_m(:,2);
+%TEMP
+time_C2_s = C2_s(:,1) + timeshift;
+data_C2_s = C2_s(:,2);
+%TEMP
+time_C2_m = C2_m(:,1) + timeshift + phase;
+data_C2_m = C2_m(:,2);
+
+%figure(1)
+%plot(time_supplycurrent_C4, supplycurrent_C4, time_motorcurrent_C4, motorcurrent_C4,time_C1_m,data_C1_m,time_C1_s,data_C1_s,time_C2_m,data_C2_m,time_C2_s,data_C2_s)
+
+figure(2)
+plot(time_supplycurrent_C4, supplycurrent_C4, time_motorcurrent_C4, motorcurrent_C4)
+
+
+ title('')
+ xlabel('Time [s]')
+ ylabel('Current [A]')
+% 
+  xlim([0 1.4e-4])
+  ylim([-5 5])
+% 
+%  
+legend('Supply','Motor')
+
+if to_tikz == 1
+    cleanfigure()
+    matlab2tikz('controllerboardv2_current_motor_supply.tex');
+end
---
+++ code/matlab/plot_shutdown/plot_shutdown.m
@@ -0,0 +1,39 @@
+clear all; close all; clc;
+
+to_tikz = 1;
+
+
+C1 = csvread('C1controller-board2-microzed-shutdown00000.txt',5,0);
+C2 = csvread('C2controller-board2-microzed-shutdown00000.txt',5,0);
+C3 = csvread('C3controller-board2-microzed-shutdown00000.txt',5,0);
+C4 = csvread('C4controller-board2-microzed-shutdown00000.txt',5,0);
+
+timeshift = -4.82e-4;
+time_C1 = C1(:,1) + timeshift;
+data_C1 = C1(:,2);
+
+time_C2 = C2(:,1) + timeshift;
+data_C2 = C2(:,2);
+
+time_C3 = C3(:,1) + timeshift;
+data_C3 = C3(:,2);
+
+time_C4 = C4(:,1) + timeshift;
+data_C4 = C4(:,2);
+
+plot(time_C1, data_C1,time_C3, data_C3,time_C2, data_C2)
+
+title('Shutdown Sequence')
+xlabel('Time [s]')
+ylabel('Voltage [V]')
+
+ xlim([0 1e-4])
+ ylim([0 3])
+
+ 
+ legend('POWER_{EN}','DCDC_{EN}','VCCIO_{EN}')
+
+if to_tikz == 1
+    cleanfigure()
+    matlab2tikz('controllerboardv2_shutdown.tex');
+end
