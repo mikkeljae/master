595b07a9cf32a242d1d7a8870edbfcdd2c4c4434
Thomas Christensen <thomas.s.christensen101@gmail.com>
2017-12-19 19:36:27 +0100

    changed plotting function for joint test data

--- data/joint_movement/data_plot.m
+++ data/joint_movement/data_plot.m
@@ -1,9 +1,6 @@
 clc;clear;close all;
 data = csvread('error_free/test1',3,1);
 
-figure()
-scatter(data(:,1),data(:,2),'.');
-
 data_overflow = data(find(data(:,1)==max(data(:,1)))+1:end,:);
 data_overflow(:,1) = data_overflow(:,1)+65535;
 data_underflow = data(1:find(data(:,1)==max(data(:,1))),:);
@@ -12,6 +9,10 @@ data = [data_underflow ; data_overflow];
 
 figure();
 scatter(data(:,1),data(:,2),'.');
+
+figure();
+scatter(data(:,2),ones(size(data(:,1))),'.');
+
 
 figure();
 %scatter(data(:,1),ones(size(data(:,1))),'.')
