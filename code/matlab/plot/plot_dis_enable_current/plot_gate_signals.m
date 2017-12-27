3484d6b8f574e3f8197d4a1fe0323e5fafdd2008
Mikkel <mikkel.jae@gmail.com>
2017-12-27 10:12:21 +0100

    fixed todo - kindof

--- code/matlab/plot/plot_dis_enable_current/plot_gate_signals.m
+++ code/matlab/plot/plot_dis_enable_current/plot_gate_signals.m
@@ -10,7 +10,8 @@ C4_load = csvread('C4start-current-rails-w-load00000.txt',5,0);
 t = C1_load(:,1);
 
 figure();
-plot(t,C1_load(:,2),t,C2_load(:,2),t,C4_load(:,2),'g')
+plot(t,C1_load(:,2),t,C2_load(:,2))
+ylim([-5 40])
 title('Current Draw on DIS Low')
 xlabel('Time [s]')
 ylabel('Voltage [V]')
@@ -18,19 +19,36 @@ legend('MGATE_BH','24V Rail','Supply Current');
 
 xlim([-1*10^-3 8*10^-3]);
 
+yyaxis right
+plot(t,C4_load(:,2),'color',[0 0.75 0])
+ylabel('Current [A]')
+ax = gca;
+c = ax.Color;
+ax.YColor = [0 0.75 0];
+xlabel('Time (s)')
+legend('MGATE_BH','24V Rail','Supply Current');
+
 if to_tikz == 1
     cleanfigure()
     matlab2tikz('current_w_load.tex');
 end
 
+
 figure();
-plot(t,C1_load(:,2),t,C2_load(:,2),t,C4_load(:,2),'g')
+plot(t,C1_load(:,2),t,C2_load(:,2))
 title('Current Draw on DIS Low')
 xlabel('Time [s]')
 ylabel('Voltage [V]')
 xlim([-0.5*10^-4 3*10^-4]);
 ylim([-5 40])
- 
+
+yyaxis right
+plot(t,C4_load(:,2),'color',[0 0.75 0])
+ylabel('Current [A]')
+ax = gca;
+c = ax.Color;
+ax.YColor = [0 0.75 0];
+
 legend('MGATE_BH','24V Rail','Supply Current');
 
 if to_tikz == 1
@@ -46,12 +64,23 @@ C4_load_slow = csvread('C4start-current-rails-w-load-slowstart00000.txt',5,0);
 t = C1_load_slow(:,1);
 
 figure();
-plot(t,C1_load_slow(:,2),t,C2_load_slow(:,2),t,C4_load_slow(:,2),'g')
+plot(t,C1_load_slow(:,2),t,C2_load_slow(:,2))
+ylim([-5 40])
 title('Current Draw on DIS Low After 4ms Wait')
 xlabel('Time [s]')
 ylabel('Voltage [V]')
 xlim([-1*10^-3 8*10^-3]);
  
+yyaxis right
+plot(t,C4_load_slow(:,2),'color',[0 0.75 0])
+ylabel('Current [A]')
+ax = gca;
+c = ax.Color;
+ax.YColor = [0 0.75 0];
+ylim([-5 40]);
+
+
+
 legend('MGATE_BH','24V Rail','Supply Current');
 
 if to_tikz == 1
